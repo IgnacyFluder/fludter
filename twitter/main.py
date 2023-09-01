625174284e729419f3a845d4881c78143aa05464
Ignacy Fluder <ignaciofluder@gmail.com>
2023-09-01 10:51:31 +0200

    Add some requirements and fix token leak

--- twitter/main.py
+++ twitter/main.py
@@ -1,11 +1,11 @@
 import tweepy
 import random
 
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAAAjbpgEAAAAAuhrmyT3Oi6nxWhqsl0hWdSoXCqA%3DyiNyZyDsoptLvTY1xlKDY3EE7jYZ4H9krr66JreR7AeJ7vfUJi'
-consumer_key = 'fI9K6GqQUwPo8FtFFJyRXBUwE'
-consumer_secret = 'WKx0PM9sEQOBiXduuzSvLA70IfGthP3QVbbUFjOnBKoZlXjiND'
-access_token = '1594793771178364928-CjQ1KDSv4o0hf8t1SuX6cU03FgSCfY'
-access_token_secret = 'MAYa44A5OSczbKZRNvmbFhU2ii7G1kUhuIW7O580fPaZu'
+bearer_token = '$'
+consumer_key = '$'
+consumer_secret = '$'
+access_token = '$'
+access_token_secret = '$'
 
 # for v1.1
 #auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
